e3fba2db5e7969bb8c4064f7eecf45eb52e80b6b
Sahal-Repo <repo@sahalmicrofinance.com>
2025-06-07 23:47:30 +0300

    second marge3

---
+++ main.js
@@ -0,0 +1,8 @@
+const net = require('net');
+
+const server = net.createServer((socket) => {
+  socket.write("HTTP/1.1 200 OK\r\n\r\n\r\n");
+  socket.end();
+});
+
+server.listen(4221, "localhost");
