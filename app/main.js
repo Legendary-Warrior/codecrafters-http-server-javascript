87aecd6fa5a9bbaa1cc3ca476766647fcde00f13
Sahal-Repo <repo@sahalmicrofinance.com>
2025-06-07 18:54:38 +0300

    Initial commit

--- app/main.js
+++ app/main.js
@@ -1,12 +1,29 @@
-const net = require("net");
+//const net = require("net");
+const http = require('http');
+;
+//const http = require('http');
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!");
-
-const server = net.createServer((socket) => {
-  socket.on("close", () => {
-    socket.end();
-  });
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.end('OK');
 });
 
-server.listen(4221, "localhost");
+server.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
+
+// You can use print statements as follows for debugging, they'll be visible when running tests.
+// console.log("Logs from your program will appear here!");
+
+// const server = net.createServer((socket) => {
+//   socket.on("close", () => {
+//     socket.end();
+//   });
+// });
+
+// server.listen(4221, "localhost");
+
+// const server = http.createServer(req,res)=>{
+//   res.statusCode= 200;
+  
+// }
