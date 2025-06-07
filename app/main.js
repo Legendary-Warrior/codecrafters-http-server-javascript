2265606fe1c577e84a051de40528a5b6044bf792
Sahal-Repo <repo@sahalmicrofinance.com>
2025-06-07 23:31:13 +0300

    second marge3

--- app/main.js
+++ app/main.js
@@ -1,28 +1,8 @@
-//const net = require("net");
-//const http = require('http');
-;
 const net = require('net');
 
 const server = net.createServer((socket) => {
-  socket.write('HTTP/1.1 200 OK\r\n\r\n\r\n');
+  socket.write("HTTP/1.1 200 OK\r\n\r\n\r\n");
   socket.end();
 });
 
-server.listen(4221, 'localhost');
-
-
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-// console.log("Logs from your program will appear here!");
-
-// const server = net.createServer((socket) => {
-//   socket.on("close", () => {
-//     socket.end();
-//   });
-// });
-
-// server.listen(4221, "localhost");
-
-// const server = http.createServer(req,res)=>{
-//   res.statusCode= 200;
-  
-// }
+server.listen(4221, "localhost");
