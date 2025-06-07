3e84ff111043352bcd2026e9aaf5f799f927e684
Sahal-Repo <repo@sahalmicrofinance.com>
2025-06-07 19:47:13 +0300

    second marge2

--- app/main.js
+++ app/main.js
@@ -4,11 +4,13 @@
 const net = require('net');
 
 const server = net.createServer((socket) => {
+  // Write the raw HTTP response as required
   socket.write('HTTP/1.1 200 OK\r\n\r\n\r\n');
   socket.end();
 });
 
 server.listen(4221, 'localhost');
+
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 // console.log("Logs from your program will appear here!");
